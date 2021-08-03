a3b2863426843fa62e17421d9226533fe4921b61
m15ha <mihail.lazareff@gmail.com>
2021-08-03 18:04:57 -0400

    vanila redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { addTodo, decrement, increment, removeLastTodo } from './vanilaRedux/mainReducer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const count = useSelector((state) => state.main.count);
+    const todos = useSelector((state) => state.main.todos);
+    const dispatch = useDispatch();
+
+    return (
+        <div className='App'>
+            <h1>Counter {count}</h1>
+            <button onClick={() => dispatch(increment())}>increment</button>
+            <button onClick={() => dispatch(decrement())}>decrement</button>
+            <button onClick={() => dispatch(removeLastTodo())}>
+                remove Last Todo
+            </button>
+            <button onClick={() => dispatch(addTodo(prompt()))}>
+                add Todo
+            </button>
+            <ul>
+                {todos.map((todo) => (
+                    <li key={todo}> {todo} </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
